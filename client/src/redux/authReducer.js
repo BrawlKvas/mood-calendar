37495222545feeb349ebad2d5f03805417eb3c16
Pukki Konstantin <pukkikonstantin@gmail.com>
2020-11-15 15:10:46 +0300

    Сохранение, удаление, инициализация токена

--- client/src/redux/authReducer.js
+++ client/src/redux/authReducer.js
@@ -1,10 +1,13 @@
 import { AuthAPI } from "../api/api"
 
 const SET_AUTH_DATA = 'SET_AUTH_DATA'
+const REMOVE_AUTH_DATA = 'REMOVE_AUTH_DATA'
+const TOKEN_STORAGE_KEY = 'TOKEN_STORAGE_KEY'
 
+const localToken = localStorage.getItem(TOKEN_STORAGE_KEY)
 const initState = {
-  token: null,
-  isAuth: false
+  token: localToken || null,
+  isAuth: !!localToken
 }
 
 const authReducer = (state = initState, action) => {
@@ -16,6 +19,12 @@ const authReducer = (state = initState, action) => {
         ...action.data,
         isAuth: true
       }
+    case REMOVE_AUTH_DATA:
+      return {
+        ...state,
+        token: null,
+        isAuth: false
+      }
 
     default:
       return state
@@ -24,12 +33,14 @@ const authReducer = (state = initState, action) => {
 
 export default authReducer
 
-const setAuthData = (data) => ({type: SET_AUTH_DATA, data})
+const setAuthData = (data) => ({ type: SET_AUTH_DATA, data })
+const removeAuthData = () => ({ type: REMOVE_AUTH_DATA })
 
 export const singIn = ({ login, password }, setStatus) => (dispatch) => {
   AuthAPI.signIn(login, password)
     .then(data => {
       dispatch(setAuthData(data))
+      localStorage.setItem(TOKEN_STORAGE_KEY, data.token)
     })
     .catch(err => {
       if (err.status === 500) {
@@ -43,7 +54,7 @@ export const singIn = ({ login, password }, setStatus) => (dispatch) => {
 export const singUp = ({ login, password }, setStatus) => (dispatch) => {
   AuthAPI.signUp(login, password)
     .then(data => {
-      setStatus({ message: data.message})
+      setStatus({ message: data.message })
     })
     .catch(err => {
       if (err.status === 500) {
@@ -52,4 +63,9 @@ export const singUp = ({ login, password }, setStatus) => (dispatch) => {
         setStatus({ error: err.data.message })
       }
     })
+}
+
+export const signOut = () => (dispatch) => {
+  localStorage.removeItem(TOKEN_STORAGE_KEY)
+  dispatch(removeAuthData())
 }
