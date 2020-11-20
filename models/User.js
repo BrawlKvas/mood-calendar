6ef06050a6ef415a756af0d2cc426f1730490996
Pukki Konstantin <pukkikonstantin@gmail.com>
2020-11-21 00:21:11 +0300

    EndPoint get month

--- models/User.js
+++ models/User.js
@@ -4,7 +4,12 @@ const schema = new Schema({
   login: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   
-  // years: [{ type: Types.ObjectId, ref: 'Year' }]
+  calendar: [
+    {
+      year: {type: Number, required: true, unique: true},
+      months: []
+    }
+  ]
 });
 
 module.exports = model('User', schema)
--- routes/main.routes.js
+++ routes/main.routes.js
@@ -1,10 +1,23 @@
 const { Router } = require('express')
 const router = Router()
 const auth = require('./../middleware/auth.middleware')
+const User = require('./../models/User')
 
-router.get('/test', auth, async (req, res) => {
+router.get('/month/:year&:month', auth, async (req, res) => {
   try {
-    res.status(201).json({message: `Ваш id ${req.user.userId}`})
+    const user = await User.findOne({ _id: req.user.userId })
+
+    const year = user.calendar.find(item => item.year === +req.params.year)
+
+    if (!year) res.status(200).json({ data: [] })
+
+    const month = year.months[req.params.month - 1]
+
+    if (!month)
+      res.status(200).json({ data: [] })
+    else
+      res.status(200).json({ data: month })
+
   } catch (error) {
     res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' })
   }
